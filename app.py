0ecdabd25131d112e479337f335ac58470204016
Aakash Vishnuvarth <121511679+aakashvishcoder@users.noreply.github.com>
2025-08-06 20:31:38 -0500

    quick fix

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def serve_video():
     return send_from_directory(VIDEO_DIR, VIDEO_FILENAME)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
